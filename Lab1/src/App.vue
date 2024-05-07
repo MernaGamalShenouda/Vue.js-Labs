21403f369a80b2f7d598434551ea0ab3806bbce4
Merna Gamal <merna.gamal.shenouda@gmail.com>
2024-05-07 17:29:26 +0300

    Lab1

---
+++ Lab1/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <NavBar v-if="!$route.meta.hideNavbar"/>
+  <div class="container">
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+
+ #app {
+  height: 100vh;
+} 
+</style>
